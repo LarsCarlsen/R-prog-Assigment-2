7cd50fd00b71e98e5b04373f2d795a53dea9385b
LarsCarlsen <LC@AwarenessCenter.dk>
2015-05-09 16:14:58 +0200

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,44 @@
+## R-programming: Assignment 2
+## INVERTING A SQUARE MATRIX APPLYING THE SOLVE FUNCTION
+
+## The first function 
+## 1: set the matrix, 
+## 2: get the matrix, 
+## 3: set the inverse matrix and 
+## 4: get the inverse matrix
+
+## The second function calculates the inverse matrix of 
+## the special "matrix" created with the first function.
+
+## Example matrix:
+## Mat<-matrix(c(3,1,6,9,4,2,6,8,9),nrow=3,ncol=3)
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(solve) m <<- solve
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+}
+
+M<-makeCacheMatrix(Mat)
+
+cacheSolve <- function(x, ...) {
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
+}
+
+cacheSolve(M)
